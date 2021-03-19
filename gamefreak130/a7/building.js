07dbc3176325aaf168b383cd20fafbe77807997d
Gamefreak130 <9084643+Gamefreak130@users.noreply.github.com>
2021-03-18 19:27:03 -0500

    Draw activity

--- gamefreak130/a7/building.js
+++ gamefreak130/a7/building.js
@@ -1,15 +1,16 @@
 class Building {
 	// Create a branching system  Each branch can hold other branches
 	constructor(aof) {
-		
 		this.aof = aof
 		this.center = new Vector()
+        this.theta = 0
 	}
 
 
 	update(t, dt) {
 		
 		// Update this with the current value of the AOF and any other parameters, like time
+        this.theta -= 0.02
 		/*this.root.energy = this.aof.energy
 		this.root.start.orientation = -Math.PI/2
 		this.root.update(t, dt)
@@ -24,23 +25,56 @@ class Building {
         let light = this.aof.get("Building Lightness")
         let height = (this.aof.get("Height")*150)+60
         let gableHeight = this.aof.get("Roof Gabling")*30
+        let activityPct = this.aof.get("Activity")
+        p.fill(hue*360, 30, light*70+30)
+        p.stroke(hue*360, 30, light*30)
+        // Draw chimney
+        p.strokeWeight(1.25)
+        p.rect(35, -height, 15, -(gableHeight*0.8 + 10))
+        // If sufficiently active, make smoke come out of the chimney
+        if (activityPct > 0.3) {
+            p.push()
+            p.translate(37.25, -(gableHeight*0.8 + height + 3))
+            p.rotate(-p.PI/2)
+            p.noFill()
+            p.stroke(0, 0, 45)
+            p.strokeWeight(2)
+            for (var i = 0; i < 3; i++) {
+                p.beginShape()
+                p.curveVertex(0, 2*p.sin(this.theta))
+                p.curveVertex(10, 2*p.sin(p.QUARTER_PI + this.theta))
+                p.curveVertex(20, 2*p.sin(p.HALF_PI + this.theta))
+                p.curveVertex(30, 2*p.sin(3*p.QUARTER_PI + this.theta))
+                p.curveVertex(40, 2*p.sin(p.PI + this.theta))
+                p.curveVertex(50, 2*p.sin(5*p.QUARTER_PI + this.theta))
+                p.endShape()
+                p.translate(0, 5)
+            }
+            p.pop()
+        }
+
+        // Draw roof
         p.fill(hue*360, 30, light*70+30)
         p.stroke(hue*360, 30, light*30)
         p.strokeWeight(6)
         p.triangle(-60, -(height - 2), 0, -(height + gableHeight - 2), 60, -(height - 2))
 
+        // Draw main structure
         p.strokeWeight(1)
 		p.rect(-60, 0, 120, -height)
 
         // Draw windows
         let numStories = Math.floor(height/50)
         let numWindows = Math.round((this.aof.get("Number of Windows")*4)+1)
-        p.fill(193, 100, 50)
+        // Based on the activity, we will light up every X windows
+        let unlitPerLit = Math.round(activityPct*-5 + 6)
         p.stroke(0)
         p.strokeWeight(0.75)
         for (var i = 0; i < numStories; i++) {
             for (var j = 0; j < numWindows; j++) {
                 let pct = numWindows == 1 ? 0.5 : j/(numWindows - 1) 
+                let isLit = unlitPerLit >= 6 ? false : (i*numWindows + j) % unlitPerLit == 0
+                p.fill(isLit ? 58 : 193, 100, 50)
                 p.rect((pct*80) - 45, (i*-50) - 10, 10, -20)
             }
         }
